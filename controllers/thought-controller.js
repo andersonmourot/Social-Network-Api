b2883460b7981cd87524ab1bfdbcb60f8342dd41
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2023-01-25 19:11:43 -0600

    Finished thought functions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -41,4 +41,51 @@ const postController = {
         res.status(500).json(err);
       });
   },
+  createPost(req, res) {
+    Post.create(req.body)
+      .then((dbPostData) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { posts: dbPostData._id } },
+          { new: true }
+        );
+      })
+      .then((dbPostData) => {
+        if (!dbPostData) {
+          return res
+            .status(400)
+            .json({ message: "Post created, but no user found" });
+        }
+        res.json({ message: "Post successful" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  deletePost(req, res) {
+    Post.findOneAndRemove({ _id: req.params.postId })
+      .then((dbPostData) => {
+        if (!dbPostData) {
+          return res.status(400).json({ message: "No post using this ID" });
+        }
+        return User.findOneAndUpdate(
+          { posts: req.params.postId },
+          { $pull: { posts: req.params.postId } },
+          { new: true }
+        );
+      })
+      .then((dbPostData) => {
+        if (!dbPostData) {
+          return res
+            .status(400)
+            .json({ message: "Post created, but no user found" });
+        }
+        res.json({ message: "Post deleted" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
