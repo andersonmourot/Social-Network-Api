7dd5f775caad35ef6f828d525ecc80a4f6a6839d
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2023-01-25 19:55:58 -0600

    Added Post model

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const responseSchema = require('./Response');
+const dateFormat = require('../utils/dateFormat');
+const { timeStamp } = require('console');
+const postSchema = new Schema(
+    {
+        postText: {
+            type: String,
+            required: 'You must write a post',
+            minlength: 1,
+            maxlength: 200
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        responses: [responseSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+postSchema.virtual('responseCount').get(function() {
+    return this.responses.length;
+});
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
