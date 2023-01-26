7953892b451aa75f8cd803d882559703b08f427d
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2023-01-25 20:01:06 -0600

    Finished response model

---
+++ models/Response.js
@@ -0,0 +1,33 @@
+const { timeStamp, time } = require('console');
+const { Schema, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const responseSchema = new Schema (
+    {
+        responseId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        responseBody: {
+            type: String,
+            required: true,
+            maxlength: 200
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+module.exports = responseSchema;
